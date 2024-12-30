d77884ad2f402854add95b71574370af4fdcd69a
giuseppesec <78179391+guusec@users.noreply.github.com>
2024-12-30 01:01:56 -0500

    Create json_escape.js

---
+++ json_escape.js
@@ -0,0 +1,9 @@
+output= JSON.stringify(input);
+var myEscapedJSONString = output.replace(/\\n/g, "\\n")
+                                      .replace(/\\'/g, "\\'")
+                                      .replace(/\\"/g, '\\"')
+                                      .replace(/\\&/g, "\\&")
+                                      .replace(/\\r/g, "\\r")
+                                      .replace(/\\t/g, "\\t")
+                                      .replace(/\\b/g, "\\b")
+                                      .replace(/\\f/g, "\\f");
